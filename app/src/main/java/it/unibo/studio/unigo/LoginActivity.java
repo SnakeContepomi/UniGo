fb57c63caf2499d4b3940292a5bcbb1a17f5d39b
Carlo Console <carlo.console@studio.unibo.it>
2017-03-14 12:04:30 +0100

    Material Design EditText implemented

--- app/src/main/java/it/unibo/studio/unigo/LoginActivity.java
+++ app/src/main/java/it/unibo/studio/unigo/LoginActivity.java
@@ -1,27 +1,83 @@
 package it.unibo.studio.unigo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener
 {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private TextInputLayout inEmail, inPass;
+    private Button btnLogin;
+    private TextView txtSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
         // Enable Fullscreen Activity
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_login);
+        initComponents();
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener()
+        {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
+            {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null)
+                {
+                    // User is signed in
+                    if (user.isEmailVerified())
+                    {
+                        //ToDo:
+                        //Toast.makeText(getApplicationContext(), "User logged in: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                        Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(myIntent);
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Email not verified", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    // User is signed out
+                }
+            }
+        };
     }
 
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        if (mAuthListener != null)
+            mAuth.removeAuthStateListener(mAuthListener);
+    }
 
     @Override
     public void onClick(View view)
@@ -29,7 +85,70 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (view.getId())
         {
             case R.id.btnLogin:
-
+                login(inEmail.getEditText().getText().toString(), inPass.getEditText().getText().toString());
+                break;
+            case R.id.txtSignUp:
+                break;
         }
+    }
+
+    private void initComponents()
+    {
+        inEmail = (TextInputLayout) findViewById(R.id.inEmail);
+        inPass = (TextInputLayout) findViewById(R.id.inPass);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        txtSignUp = (TextView) findViewById(R.id.txtSignUp);
+    }
+
+    private void login(String email, String password)
+    {
+        /*mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (user != null) {
+                            user.sendEmailVerification()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful())
+                                            {
+                                                Log.d("INFO", "Email sent.");
+                                            }}
+                                    });
+                        }
+                        Toast.makeText(getApplicationContext(), "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(), "registration failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });*/
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(), "login failed", Toast.LENGTH_SHORT).show();
+
+                            inPass.setErrorEnabled(true);
+                            inPass.setError(getResources().getString(R.string.error_login));
+                        }
+                    }
+                });
     }
 }
