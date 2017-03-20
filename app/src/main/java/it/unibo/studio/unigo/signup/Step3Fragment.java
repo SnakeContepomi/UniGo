f2905dc1616bc03ce52b4235885465eb0ae04366
Riki <riccardo.lucchi3@gmail.com>
2017-03-20 21:00:06 +0100

    Step 3 Fragment GUI pt1

--- app/src/main/java/it/unibo/studio/unigo/signup/Step3Fragment.java
+++ app/src/main/java/it/unibo/studio/unigo/signup/Step3Fragment.java
@@ -6,20 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 import it.unibo.studio.unigo.R;
 
 public class Step3Fragment extends Fragment implements Step
 {
+
+    private String[] REGIONS;
+
+    private MaterialBetterSpinner spRegion, spUni, spSchool;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_step3, container, false);
 
-        //initialize your UI
+        initializeComponents(v);
 
         return v;
     }
@@ -39,4 +46,22 @@ public class Step3Fragment extends Fragment implements Step
     public void onError(@NonNull VerificationError error) {
         //handle error inside of the fragment, e.g. show error on EditText
     }
+
+    private void initializeComponents(View v)
+    {
+        REGIONS = getResources().getStringArray(R.array.regions);
+
+        ArrayAdapter<String> arrayAdapterRegion = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, REGIONS);
+        spRegion = (MaterialBetterSpinner) v.findViewById(R.id.spinner_region);
+        spRegion.setAdapter(arrayAdapterRegion);
+
+        ArrayAdapter<String> arrayAdapterUni = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, REGIONS);
+        spRegion = (MaterialBetterSpinner) v.findViewById(R.id.spinner_uni);
+        spRegion.setAdapter(arrayAdapterUni);
+
+        ArrayAdapter<String> arrayAdapterSchool = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, REGIONS);
+        spRegion = (MaterialBetterSpinner) v.findViewById(R.id.spinner_school);
+        spRegion.setAdapter(arrayAdapterSchool);
+    }
+
 }
