26c8bd0288b960a26dd774a044ae029fc0a1d96e
Riki <riccardo.lucchi3@gmail.com>
2017-03-28 15:23:04 +0200

    bug fixes

--- app/src/main/java/it/unibo/studio/unigo/signup/Step1Fragment.java
+++ app/src/main/java/it/unibo/studio/unigo/signup/Step1Fragment.java
@@ -13,8 +13,6 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
