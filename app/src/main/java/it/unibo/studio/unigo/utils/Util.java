3a5dcded35d22b4aa1e43dcdf4f7ba77f861dbde
Riki <riccardo.lucchi3@gmail.com>
2017-03-28 15:57:03 +0200

    bug fixes

--- app/src/main/java/it/unibo/studio/unigo/utils/Util.java
+++ app/src/main/java/it/unibo/studio/unigo/utils/Util.java
@@ -3,9 +3,23 @@ package it.unibo.studio.unigo.utils;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Util
 {
+    private static FirebaseDatabase database;
+
+    // Riferimento al database di Firebase
+    public static FirebaseDatabase getDatabase()
+    {
+        if (database == null)
+        {
+            database = FirebaseDatabase.getInstance();
+            database.setPersistenceEnabled(true);
+        }
+        return database;
+    }
+
     // Ritorna true il dispositivo è connesso ad internet, false altrimenti
     public static boolean isNetworkAvailable(Context context)
     {
