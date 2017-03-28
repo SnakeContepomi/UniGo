57ff950ab3d7c01916d2281ccb64832f149111b8
Riki <riccardo.lucchi3@gmail.com>
2017-03-28 15:37:13 +0200

    bug fixes

--- app/src/main/java/it/unibo/studio/unigo/main/ProfileFragment.java
+++ app/src/main/java/it/unibo/studio/unigo/main/ProfileFragment.java
@@ -10,8 +10,7 @@ import it.unibo.studio.unigo.R;
 public class ProfileFragment extends Fragment
 {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
