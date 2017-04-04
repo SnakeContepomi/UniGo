bb2982f6eb814240e2ecde9c703d0697d3adbde0
Riki <riccardo.lucchi3@gmail.com>
2017-04-04 15:05:03 +0200

    post activity modified

--- app/src/main/java/it/unibo/studio/unigo/main/PostActivity.java
+++ app/src/main/java/it/unibo/studio/unigo/main/PostActivity.java
@@ -4,13 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.LinearLayout;
 import it.unibo.studio.unigo.R;
 
 public class PostActivity extends AppCompatActivity
 {
-    private LinearLayout rootLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,8 +18,6 @@ public class PostActivity extends AppCompatActivity
 
     private void initComponents()
     {
-        rootLayout = (LinearLayout) findViewById(R.id.profileLayout);
-
         // Inizializzazione Toolbar
         Toolbar mActionBar = (Toolbar) findViewById(R.id.PostToolbar);
         mActionBar.setNavigationOnClickListener(new View.OnClickListener() {
