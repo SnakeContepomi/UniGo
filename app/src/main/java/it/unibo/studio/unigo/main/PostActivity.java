2322813814e8ec0127be2c0013b53bc0c8eaf861
Riki <riccardo.lucchi3@gmail.com>
2017-04-04 16:01:38 +0200

    post toolbar with button

--- app/src/main/java/it/unibo/studio/unigo/main/PostActivity.java
+++ app/src/main/java/it/unibo/studio/unigo/main/PostActivity.java
@@ -3,10 +3,13 @@ package it.unibo.studio.unigo.main;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
 import it.unibo.studio.unigo.R;
 
-public class PostActivity extends AppCompatActivity
+public class PostActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,13 +22,27 @@ public class PostActivity extends AppCompatActivity
     private void initComponents()
     {
         // Inizializzazione Toolbar
-        Toolbar mActionBar = (Toolbar) findViewById(R.id.PostToolbar);
-        mActionBar.setNavigationOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.PostToolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 onBackPressed();
             }
         });
+        toolbar.inflateMenu(R.menu.post_activity_toolbar);
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.post_toolbar_send:
+                Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
