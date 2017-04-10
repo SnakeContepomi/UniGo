aaa2046d7df9858ae45facba24b5177faba54579
Carlo Console <carlo.console@studio.unibo.it>
2017-04-10 10:34:58 +0200

    Recycler Item Question Finished

--- app/src/main/java/it/unibo/studio/unigo/main/HomeFragment.java
+++ app/src/main/java/it/unibo/studio/unigo/main/HomeFragment.java
@@ -3,21 +3,27 @@ package it.unibo.studio.unigo.main;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.github.fabtransitionactivity.SheetLayout;
 import it.unibo.studio.unigo.R;
+import it.unibo.studio.unigo.utils.QuestionAdapter;
 
 public class HomeFragment extends Fragment
 {
     private FloatingActionButton fab;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-        initComponents();
+        initComponents(v);
 
         return v;
     }
@@ -36,7 +42,7 @@ public class HomeFragment extends Fragment
         fab.show();
     }
 
-    private void initComponents()
+    private void initComponents(View v)
     {
         fab = (FloatingActionButton) getActivity().findViewById(R.id.fabHome);
         fab.setOnClickListener(new View.OnClickListener(){
@@ -46,5 +52,18 @@ public class HomeFragment extends Fragment
                 ((SheetLayout) getActivity().findViewById(R.id.bottom_sheet)).expandFab();
             }
         });
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recyclerViewQuestion);
+
+        // Impostazione di ottimizzazione da usare se gli elementi non comportano il ridimensionamento della RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(v.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Inizializzazione adapter della lista delle domande
+        mAdapter = new QuestionAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 }
