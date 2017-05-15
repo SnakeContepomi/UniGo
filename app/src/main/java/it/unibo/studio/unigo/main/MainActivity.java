5e979d7772a1a1b0e7ed2d4b8d5fce398123bcd9
Carlo Console <carlo.console@studio.unibo.it>
2017-05-15 09:43:52 +0200

    Implementing New SearchView

--- app/src/main/java/it/unibo/studio/unigo/main/MainActivity.java
+++ app/src/main/java/it/unibo/studio/unigo/main/MainActivity.java
@@ -11,11 +11,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import com.github.fabtransitionactivity.SheetLayout;
 import com.github.pwittchen.reactivenetwork.library.ReactiveNetwork;
 import com.google.firebase.auth.FirebaseAuth;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
@@ -65,6 +68,7 @@ public class MainActivity extends AppCompatActivity implements SheetLayout.OnFab
     private InfoFragment fragmentInfo;
 
     private Toolbar toolbar;
+    private MaterialSearchView searchView;
     private ProfileDrawerItem profile;
     private AccountHeader header;
     private Drawer navDrawer;
@@ -170,6 +174,17 @@ public class MainActivity extends AppCompatActivity implements SheetLayout.OnFab
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_item_search, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
+
     // Creazione della nuova activity con animazione
     @Override
     public void onFabAnimationEnd()
@@ -180,6 +195,37 @@ public class MainActivity extends AppCompatActivity implements SheetLayout.OnFab
 
     private void initComponents()
     {
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown()
+            {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed()
+            {
+                //Do some magic
+            }
+        });
+
         // Inizializzazione dei componenti utilizzati per l'animazione del fab
         sheetLayout = (SheetLayout) findViewById(R.id.bottom_sheet);
         fab = (FloatingActionButton) findViewById(R.id.fabHome);
