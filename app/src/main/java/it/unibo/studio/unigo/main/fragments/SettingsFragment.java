83aa8bb81e486c9ebdb3ee26a15c605a03707d2c
Riki <riccardo.lucchi3@gmail.com>
2017-05-25 11:48:26 +0200

    Fragment type changed

--- app/src/main/java/it/unibo/studio/unigo/main/fragments/SettingsFragment.java
+++ app/src/main/java/it/unibo/studio/unigo/main/fragments/SettingsFragment.java
@@ -1,29 +1,14 @@
 package it.unibo.studio.unigo.main.fragments;
 
 import android.os.Bundle;
-import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.XpPreferenceFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import it.unibo.studio.unigo.R;
 
 public class SettingsFragment extends XpPreferenceFragment
 {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
-    }
-
-    @Override
     public void onCreatePreferences2(Bundle savedInstanceState, String rootKey)
     {
-        // Add 'notifications' preferences, and a corresponding header.
-        PreferenceCategory fakeHeader = new PreferenceCategory(getPreferenceManager().getContext());
-        fakeHeader.setTitle("Notifiche");
-        getPreferenceScreen().addPreference(fakeHeader);
         addPreferencesFromResource(R.xml.preferences);
     }
 }
