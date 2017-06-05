1f28166937c78a988ef213602d75e5be2c31ca2c
Riki <riccardo.lucchi3@gmail.com>
2017-06-05 16:11:08 +0200

    bounce effect added

--- app/src/main/java/it/unibo/studio/unigo/main/fragments/InfoFragment.java
+++ app/src/main/java/it/unibo/studio/unigo/main/fragments/InfoFragment.java
@@ -12,6 +12,8 @@ public class InfoFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View v = inflater.inflate(R.layout.fragment_info, container, false);
+
+        return v;
     }
 }
