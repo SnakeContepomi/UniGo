4581c74da8f27d8adedcd69a611e94da3100beab
Carlo Console <carlo.console@studio.unibo.it>
2017-09-08 11:19:32 +0200

    Minor Fix

--- app/src/main/java/it/unibo/studio/unigo/signup/Step3Fragment.java
+++ app/src/main/java/it/unibo/studio/unigo/signup/Step3Fragment.java
@@ -13,8 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
-
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
--- app/src/main/java/it/unibo/studio/unigo/utils/BackgroundService.java
+++ app/src/main/java/it/unibo/studio/unigo/utils/BackgroundService.java
@@ -18,13 +18,8 @@ import android.graphics.Rect;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.IBinder;
-import android.support.annotation.NonNull;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.preference.PreferenceManager;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
